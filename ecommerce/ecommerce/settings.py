b83cb584a020d83176695df0bcab5fb83bf19b88
James A Ridley <jrid88@gmail.com>
2021-08-18 19:26:19 -0400

    Added new functionality

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -124,6 +124,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT_DIRS = os.path.join(BASE_DIR, 'static/images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
